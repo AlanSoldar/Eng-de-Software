ac487bf8444c929cc07c27d051057796c77a532a
alansoldar <alan.soledar@hotmail.com>
2020-08-21 22:37:13 -0300

    criando aluno

--- src/main/java/com/example/entities/Aluno.java
+++ src/main/java/com/example/entities/Aluno.java
@@ -1,5 +1,24 @@
 package com.example.entities;
 
+import com.sun.istack.NotNull;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Aluno {
+
+    @Id
+    @NotNull
+    Long id;
+
+    @Column(name = "ALUNO_NOME")
+    @NotNull
+    private String nome;
+
+    @Column(name = "ALUNO_MATRICULA")
+    @NotNull
+    private Long matricula;
+
 }
